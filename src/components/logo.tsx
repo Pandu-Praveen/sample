2ccb981acfa76e3f58c976f61137784950e05026
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2024-08-26 18:37:39 +0530

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 import { appConfig } from "@/config/app";
 import { Icons } from "./icons";
-import sampleLogo from '../../src/img/sample.png';
+import headerLogo from '../img/sample.png';
 export function Logo() {
   return (
     <>

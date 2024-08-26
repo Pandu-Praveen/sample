1808d52fd17b2e074a5755a2d0e0d39cb59c271e
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2024-08-26 18:33:46 +0530

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
 import { appConfig } from "@/config/app";
 import { Icons } from "./icons";
-
+import sampleLogo from '../../src/img/sample.png';
 export function Logo() {
   return (
     <>
       {/*<Icons.logo className="h-6 w-6" /> */}
-      <img src="/src/img/sample.png" alt="" width={50} />
+      <img src="sampleLogo" alt="" width={50} />
       <span className="font-bold ">{appConfig.name}</span>
     </>
   );
